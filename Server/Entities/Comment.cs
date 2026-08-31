0aadfbaab07a101d1a4449ecc768db088a024557
Mszur04 <jestemtunowy@outlook.com>
2026-08-31 11:46:55 +0200

    yes

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
